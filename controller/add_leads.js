fef82a70f2e1511f7333c73374742a7a48c668ab
Devnaagar <sunnynaagar773@gmail.com>
2024-06-17 17:41:42 +0530

    somechanges

--- controller/add_leads.js
+++ controller/add_leads.js
@@ -5,6 +5,7 @@ import connection from '../db/connectdb.js';
 class Add_leads{
     static add_lead= (req, res) => {
         const { lead_name, lead_email, lead_phone } = req.body;
+
         const query = 'INSERT INTO leads (lead_name,email,phone) VALUES (?, ?, ?)';
         connection.query(query, [lead_name, lead_email, lead_phone], (err, result) => {
             if (err) throw err;
